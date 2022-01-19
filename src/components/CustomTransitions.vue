62c221af43b6e2c0d1d03efeebeffb0c1a10d818
Thomas Ecalle <thomasecalle@gmail.com>
2022-01-19 15:17:29 +0100

    Handle animations with animjs

--- src/components/CustomTransitions.vue
+++ src/components/CustomTransitions.vue
@@ -1,80 +1,51 @@
 
 <script setup>
 
+    import anime from 'animejs';
  // --------
   // ENTERING
   // --------
 
   const beforeEnter = function (el) {
-    console.log("before Enter");
-    const totos = el.getElementsByClassName("toto");
-    for(var i = 0; i < totos.length; i++) {
-        const element = totos[i];
-        element.style.opacity = 0;
-    }
+    
   };
 
   // the done callback is optional when
   // used in combination with CSS
   const enter = async function (el, done) {
-    console.log("Enter");
-    const totos = el.getElementsByClassName("toto");
-
-    for(var i = 0; i < totos.length; i++) {
-        totos[i].animate([
-        // keyframes
-        { transform: 'translateY(30px)', opacity: 0 },
-        { transform: 'translateY(0)', opacity: 1 }
-        ], {
-            // timing options
-            duration: 600,
-            iterations: 1
-        });
-        totos[i].style.opacity = 1;
-        await timeout(300);
-    }
-
-    done()
+    anime({
+        targets: el.getElementsByClassName("toto"),
+        translateY: [30, 0],
+        opacity: [0, 1],
+        easing: 'easeInQuad',
+        duration: 600,
+        delay: anime.stagger(300),
+        complete: done,
+    });
   };
 
-  const afterEnter = function (el) {
-     console.log("After Enter");
-        
-  };
+  const afterEnter = function (el) {};
 
-  const enterCancelled = function (el) {
-     console.log("Enter Cancelled");
-  };
+  const enterCancelled = function (el) {};
 
   // --------
   // LEAVING
   // --------
 
-  const beforeLeave =function (el) {
-    console.log("Before leave");
-  };
+  const beforeLeave =function (el) {};
 
   // the done callback is optional when
   // used in combination with CSS
   const leave = async function (el, done) {
-    
-    const totos = el.getElementsByClassName("toto");
-
-    for(var i = 0; i < totos.length; i++) {
-        totos[i].animate([
-        // keyframes
-        { transform: 'translateY(0px)', opacity: 1 },
-        { transform: 'translateY(-30px)', opacity: 0 }
-        ], {
-            // timing options
-            duration: 600,
-            iterations: 1
-        });
-        totos[i].style.opacity = 0;
-        await timeout(300);
-    }
-
-    done()
+    anime({
+        targets: el.getElementsByClassName("toto"),
+        translateY: [0, -30],
+        opacity: [1, 0],
+        easing: 'easeOutQuad',
+        duration: 600,
+        delay: anime.stagger(300),
+        complete: done,
+    });
   };
 
   const afterLeave = function (el) {};
