39bdf3abda7303579a6afbe710aa57bd15555b39
Thomas Ecalle <thomasecalle@gmail.com>
2022-01-19 22:10:27 +0100

    Put videos on side for the moment

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
-import { ref } from 'vue'
 import AppBar from './components/AppBar.vue';
 import BottomBar from './components/BottomBar.vue';
 import Home from './components/Home.vue';
@@ -30,10 +29,19 @@ function onModeClicked() {
 
 <template>
   <div>
-    <video class="absolute w-full h-full object-cover" autoplay muted loop>
-      <source src="./assets/test.mp4" type="video/mp4">
-    </video>
-    <div class="absolute w-full h-full bg-[#ddd2bc] opacity-30" />
+
+    <!--<transition name="fade" mode="out-in">
+      <video v-show="!store.state.darkMode" class="absolute w-full h-full object-cover" autoplay muted loop>
+        <source src="./assets/beige_1080_adjusted.mov" type="video/mp4">
+      </video>
+    </transition>-->
+
+    <!--<transition name="fade" mode="out-in">
+      <div v-show="!store.state.darkMode" class="absolute w-full h-full bg-[#ddd2bc] opacity-50" />
+    </transition>-->
+
+    <div class="absolute w-full h-full bg-[#ddd2bc]" />
+
     <div class="absolute z-2 h-screen w-screen flex flex-col justify-around p-[25px]">
       <AppBar
           v-bind:onMenuClicked="onMenuClicked"
