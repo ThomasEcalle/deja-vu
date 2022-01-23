e348352f9664468d7486ac85d0c5fa6acd07b468
Thomas Ecalle <thomasecalle@gmail.com>
2022-01-23 17:24:32 +0100

    Put About on left and resize description

--- src/components/OtherDetail.vue
+++ src/components/OtherDetail.vue
@@ -14,10 +14,10 @@ const selectedOther = store.getters.getSelectedOther;
         class="absolute h-full w-full pl-20 flex flex-col items-center justify-center dark:text-white text-black font-normal transition-colors duration-1000"
         :id="PAGE_OTHER"
     >
-        <div class="h-full w-full flex justify-center items-center">
-            <div class=" w-[60%]">
+        <div class="h-full w-full flex items-center">
+            <div class=" w-[70%]">
                 <h2 id="other-detail-title" class="stroked text-[5.5vh] font-lemonmilk">{{ selectedOther.title }}</h2>
-                <div id="other-detail-description" class="text-[1.7vh]" font-normal v-html="selectedOther.description"></div>
+                <div id="other-detail-description" class="text-[1.85vh] text-justify" font-normal v-html="selectedOther.description"></div>
             </div>
         </div>
     </div>
