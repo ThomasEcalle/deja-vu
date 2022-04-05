7b45daee3f30fb280eab49403f98961eec1a3c10
Thomas Ecalle <thomasecalle@gmail.com>
2022-04-05 14:53:58 +0200

    Replace old categories' title by new ones

--- src/i18n.js
+++ src/i18n.js
@@ -15,8 +15,8 @@ const messages = {
   "en-US": {
     homeTitle1: "We transcend the borders between space",
     homeTitle2: "and time by creating immersive experiences",
-    b2bTitle: "Professionals",
-    b2cTitle: "Individuals",
+    b2bTitle: "Immersive content",
+    b2cTitle: "Time capsule",
     contactButton: "Contact - us  -  Contact - us  -  ",
     contactFormTitle: "Contact us",
     firstNameLabel: "First name",
@@ -35,8 +35,8 @@ const messages = {
   "fr-FR": {
     homeTitle1: "Nous abolissons les frontières de l'espace",
     homeTitle2: "et du temps en créant des expériences immersives",
-    b2bTitle: "Professionnels",
-    b2cTitle: "Particuliers",
+    b2bTitle: "Contenu immersif ",
+    b2cTitle: "Capsule temporelle",
     contactButton: "Contactez- nous   -   Contactez - nous    -   ",
     contactFormTitle: "Contactez-nous",
     firstNameLabel: "Prénom",
